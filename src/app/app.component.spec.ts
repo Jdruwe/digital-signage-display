79f1d38e32a38f889611cb93ddb17550a2a70f38
Pieter-JanKdg <pieterjan.noe@studenkt.kdg.be>
2019-04-05 14:33:59 +0200

    HttpClientTestingModule added to app test class

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,12 @@
 import {async, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [
         AppComponent
       ],
