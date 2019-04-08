a00c0cd4fa7afa71536017a98deae8d5deb2eef5
Philippe <philippecappelle1@gmail.com>
2019-04-08 17:01:38 +0200

    Added speaker name and twitter

--- src/app/components/room-details/speaker/speaker.component.ts
+++ src/app/components/room-details/speaker/speaker.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Speaker} from '../../../models/speaker';
 
 @Component({
   selector: 'app-speaker',
   templateUrl: './speaker.component.html',
   styleUrls: ['./speaker.component.scss']
 })
-export class SpeakerComponent implements OnInit {
+export class SpeakerComponent {
 
-  constructor() { }
+  @Input() speaker: Speaker;
 
-  ngOnInit() {
+  constructor() {
   }
 
 }
