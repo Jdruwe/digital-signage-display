c39f93eb86ede9b84efba7d2958dcf2a5fc6f93a
Pieter-JanKdg <pieterjan.noe@studenkt.kdg.be>
2019-04-09 12:08:27 +0200

    added tests

--- src/app/components/settings/settings.component.spec.ts
+++ src/app/components/settings/settings.component.spec.ts
@@ -1,6 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
-import { SettingsComponent } from './settings.component';
+import {SettingsComponent} from './settings.component';
+import {FormsModule} from '@angular/forms';
+import {NavbarComponent} from '../navbar/navbar.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -8,9 +13,17 @@ describe('SettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [
+        SettingsComponent,
+        NavbarComponent
+      ],
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +35,24 @@ describe('SettingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('settings should have title Settings', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Settings');
+  });
+
+  it('should render 2 input fields ', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('input').length).toBe(2);
+  });
+
+
+  it('should call onSubmit method', fakeAsync(() => {
+    spyOn(component, 'updateSettings');
+    fixture.debugElement.query(By.css('form')).triggerEventHandler('submit', null);
+    expect(component.updateSettings).toHaveBeenCalled();
+  }));
+
 });
