adc2df4b56d17c25d220d88ca353187d7dd1c208
Pieter-JanKdg <pieterjan.noe@studenkt.kdg.be>
2019-05-24 14:43:43 +0200

    fixed settings bug

--- src/app/components/admin/settings/settings.component.ts
+++ src/app/components/admin/settings/settings.component.ts
@@ -26,6 +26,7 @@ export class SettingsComponent implements OnInit, OnDestroy {
     this.settingsService.getSettings()
       .subscribe(data => {
         this.settings = data;
+        console.log(this.settings);
         this.newTime(this.settings.minutesBeforeNextSession);
       });
     this.timeSub = this.settingsService.currentTimeBefore
@@ -46,12 +47,16 @@ export class SettingsComponent implements OnInit, OnDestroy {
 
   updateSettings(form: NgForm) {
     if (form.valid) {
-      this.settingsService.changeSettings(form.value.minutesBeforeNextSession, form.value.showOccupancyCounter)
+      this.settingsService.changeSettings(
+        form.value.minutesBeforeNextSession,
+        form.value.mailDelayForConnectionIssues,
+        form.value.showOccupancyCounter)
         .subscribe((response: MainSettings) => {
           this.newTime(form.value.minutesBeforeNextSession);
           this.showMessage('Settings saved!');
           this.settings.minutesBeforeNextSession = response.minutesBeforeNextSession;
           this.settings.roomOccupancyOn = response.roomOccupancyOn;
+          this.settings.mailDelayForConnectionIssues = response.mailDelayForConnectionIssues;
         }, error => {
           this.showMessage('Failed, please try again.');
         });
