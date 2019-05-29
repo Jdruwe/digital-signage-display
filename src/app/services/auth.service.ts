56db5d8f909dcc95bd8d8875592209da7e613454
Pieter-JanKdg <pieterjan.noe@studenkt.kdg.be>
2019-05-29 14:51:09 +0200

    changed .prod in imports

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {environment} from '../../environments/environment.prod';
+import {environment} from '../../environments/environment';
 import {Admin} from '../models/admin';
 import {HttpClient} from '@angular/common/http';
 import {Login} from '../models/login';
